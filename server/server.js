b51adc38a3bd77b3b0489cbfbc5bc12a0b206067
Himesh <himesh.isanka@outlook.com>
2026-03-11 16:04:14 +0530

    Fix CORS issue

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: ['http://localhost:5173',"https://mern-travel-hub.vercel.app"],
   credentials: true,
 }));
 app.use(express.json());
